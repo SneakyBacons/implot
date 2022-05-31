76a3b5269813971e76e4609d50ab18d8853235c8
Scott Balfour <scott2004b@hotmail.com>
2022-05-31 07:34:50 +0100

    [~] Adding in premake

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+project "ImPlot"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+  targetdir("bin/%{prj.name}/" .. outputdir )
+  objdir("bin-int/%{prj.name}/" .. outputdir )
+
+  IncludeDir["ImPlot"] = os.realpath(".")
+  
+	files
+	{        
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+    ToInclude = {}
+    for k,i in pairs(IncludeDir) do 
+        ToInclude[#ToInclude + 1] = i
+    end
+
+    includedirs
+    {
+        ToInclude
+    }
+
+    links
+    {
+        "ImGui",
+    }
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:MemoryProfile"
+	  	runtime "Release"
+	  	optimize "on"
+
+	filter "configurations:Development"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
